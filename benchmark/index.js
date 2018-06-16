736a71b11049ba0abe1a2e9845d11be8a9eda882
Raphael Thomazella <thomazella9@gmail.com>
2018-06-16 13:36:00 -0300

    improved benchmark organization

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,18 +3,21 @@ const { getElementProps } = require("../dist/index");
 const { getEventProps } = require("../dist/index");
 const { getGlobalProps } = require("../dist/index");
 const Benchmark = require("benchmark");
-const suite = new Benchmark.Suite("All methods");
 
-suite
+const getall = new Benchmark.Suite("getAllProps()");
+getall
   .add("getAllProps", function() {
     void getAllProps();
   })
-  .add("getElementProps('label')", function() {
-    void getElementProps("label");
+  .on("cycle", function(event) {
+    console.log(String(event.target)); //eslint-disable-line no-console
   })
-  .add("getEventProps()", function() {
-    void getEventProps();
-  })
+  .on("start", function() {
+    console.log(`Benchmarking ${this.name}`); //eslint-disable-line no-console
+  });
+
+const getglobal = new Benchmark.Suite("getGlobalProps()");
+getglobal
   .add("getGlobalProps()", function() {
     void getGlobalProps();
   })
@@ -22,6 +25,36 @@ suite
     console.log(String(event.target)); //eslint-disable-line no-console
   })
   .on("start", function() {
-    console.group(`Benchmarking ${this.name}`); //eslint-disable-line no-console
+    console.log(`Benchmarking ${this.name}`); //eslint-disable-line no-console
+  });
+
+const getelement = new Benchmark.Suite("getElementProps('label')");
+getelement
+  .add("getElementProps('label')", function() {
+    void getElementProps("label");
   })
-  .run({ async: true });
+  .on("cycle", function(event) {
+    console.log(String(event.target)); //eslint-disable-line no-console
+  })
+  .on("start", function() {
+    console.log(`Benchmarking ${this.name}`); //eslint-disable-line no-console
+  });
+
+const getevent = new Benchmark.Suite("getEventProps()");
+getevent
+  .add("getEventProps()", function() {
+    void getEventProps();
+  })
+  .on("cycle", function(event) {
+    console.log(String(event.target)); //eslint-disable-line no-console
+  })
+  .on("start", function() {
+    console.log(`Benchmarking ${this.name}`); //eslint-disable-line no-console
+  });
+
+const suites = [];
+suites.push(getall);
+// suites.push(getelement);
+// suites.push(getevent);
+// suites.push(getglobal);
+suites.forEach(su => su.run({ async: true }));
